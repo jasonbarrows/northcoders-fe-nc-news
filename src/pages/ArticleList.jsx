3bbff96fc025e61280ccc3cdf3f7a79449a7f583
Jason Barrows <jasonbarrows@gmail.com>
2023-07-18 13:22:24 +0100

    Add loading state to articles page

--- src/pages/ArticleList.jsx
+++ src/pages/ArticleList.jsx
@@ -3,11 +3,13 @@ import { getAllArticles } from "../api";
 import ArticleCard from "../components/ArticleCard";
 
 const ArticleList = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
     getAllArticles().then(({ articles }) => {
       setArticles(articles);
+      setIsLoading(false);
     });
   }, []);
 
@@ -15,16 +17,19 @@ const ArticleList = () => {
     <div className="sm:my-8 sm:max-w-2xl mx-auto">
       <h2 className="m-3 text-xl font-medium">Articles</h2>
       <ul className=" flex flex-col">
-        <li></li>
-        {
-          articles.map((article) => {
-            return (
-              <li className="border-t-4 sm:border-t-0" key={article.article_id}>
-                <ArticleCard article={article} />
-              </li>
-            );
-          })
-        }
+      {
+        isLoading
+          ? <li className="m-3">Loading...</li>
+          : (
+              articles.map((article) => {
+                return (
+                  <li className="border-t-4 sm:border-t-0" key={article.article_id}>
+                    <ArticleCard article={article} />
+                  </li>
+                );
+              })
+            )
+          }
       </ul>
     </div>
   );
