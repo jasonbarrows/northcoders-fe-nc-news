5a25666cc93c63690f564f6b0f7baac3ac1ca7ca
Jason Barrows <jasonbarrows@gmail.com>
2023-07-21 10:46:35 +0100

    Add loading state to delete button

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -52,15 +52,23 @@ const CommentCard = ({ comment, user, setComments }) => {
           ? <button
               onClick={handleDeleteComment}
               disabled={isDeleting}
-              className={`px-4 py-1 text-sm sm:text-base font-medium leading-5 sm:leading-5 rounded-full text-fuchsia-700 border border-fuchsia-200 ${isDeleting ? null : 'hover:border-fuchsia-700 hover:bg-fuchsia-100 active:text-fuchsia-800 active:bg-fuchsia-200'}`}
-            >Delete</button>
+              className={`px-4 py-1 inline-flex items-center justify-center text-sm sm:text-base font-medium leading-5 sm:leading-5 rounded-full text-fuchsia-700 border border-fuchsia-200 ${isDeleting ? null : 'hover:border-fuchsia-700 hover:bg-fuchsia-100 active:text-fuchsia-800 active:bg-fuchsia-200'}`}
+            >
+              {
+                isDeleting
+                && <svg class="animate-spin mr-2 h-4 w-4 sm:h-5 sm:w-5 text-fuchsia-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                  <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+              }
+              { isDeleting ? 'Deleting...' : 'Delete' }
+            </button>
           : null
         }
       </div>
       {
         hasDeleteError
-        ? <p className="text-right text-xs sm:text-sm text-fuchsia-500">There was an issue deleting your comment.</p>
-        : null
+        && <p className="text-right text-xs sm:text-sm text-fuchsia-500">There was an issue deleting your comment.</p>
       }
     </div>
   );
